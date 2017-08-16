d1e7da132dedd854706e969de37bcb16e8ee4491
siliconmagi <siliconmagi@yandex.com>
2017-08-16 01:37:06 -0700

    alt constructor

--- oop.py
+++ oop.py
@@ -29,19 +29,31 @@ class Employee:
         self.pay = int(self.pay * self.raise_amount)
 
     # classmethod
-    # receives class 'cls' as 1st argument
+    # receives class 'cls' as 1st argument, 'amount' as param
     # instead of instance 'self'
     @classmethod
     def set_raise_amt(cls, amount):
-        cls.raise_amount = amount
+        cls.raise_amt = amount
+
+    # alternative constructor, accepts employee string
+    @classmethod
+    def from_str(cls, empStr):
+        first, last, pay = empStr.split('-')
+        return cls(first, last, pay)
 
 
 # initialize employees
 # added to num_of_emp
 emp1 = Employee('Chrono', 'Trigger', 9000)
 emp2 = Employee('Xeno', 'Gears', 10000)
+emp_str_1 = 'Chrono-Trigger-9000'
+emp_str_2 = 'Xeno-Gears-10000'
+emp3 = Employee.from_str(emp_str_1)
 
+# edit instance variable
 emp1.raise_amount = 1.05
+
+Employee.set_raise_amt(1.05)
 
 # emp1 inherets above assignment
 # emp2 inherets class variable
@@ -50,3 +62,8 @@ print(Employee.raise_amount)
 print(emp1.raise_amount)
 print(emp2.raise_amount)
 print(Employee.num_of_emp)
+print(Employee.raise_amt)
+print(emp1.raise_amt)
+print(emp2.raise_amt)
+print(emp3.email)
+print(emp3.pay)
