5f2be30525feebfcd27c1d9967748bf310e6942b
siliconmagi <siliconmagi@yandex.com>
2017-08-23 23:12:23 -0700

    2017-08-23 23:12:23

--- dateTime.py
+++ dateTime.py
@@ -1,3 +1,5 @@
+import os
 from time import localtime, strftime
 time = strftime("%Y-%m-%d %H:%M:%S", localtime())
-print(time)
+cmd = 'echo ' + time
+os.system(cmd)
--- gitPy.py
+++ gitPy.py
@@ -1,7 +1,7 @@
 import os
 from time import localtime, strftime
 time = strftime("%Y-%m-%d %H:%M:%S", localtime())
-
+cmd = 'git commit -m "' + time + '"'
 print(os.getcwd())
 
 # rsync dotfiles to folder
@@ -10,5 +10,5 @@ print(os.getcwd())
 
 # push changes to github
 os.system('git add -A')
-os.system('git commit -m "time"')
+os.system(cmd)
 os.system('git push')
